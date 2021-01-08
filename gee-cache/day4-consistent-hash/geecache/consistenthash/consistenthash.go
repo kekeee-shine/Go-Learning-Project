363be35877cacffc9ea284abc040621260becf5d
yangfan <fan.yang@polixir.ai>
2021-01-08 17:14:27 +0800

    gee cache部分学习完

---
+++ gee-cache/day4-consistent-hash/geecache/consistenthash/consistenthash.go
@@ -0,0 +1,61 @@
+package consistenthash
+
+import (
+	"hash/crc32"
+	"sort"
+	"strconv"
+)
+
+// Hash maps bytes to uint32
+type Hash func(data []byte) uint32
+
+// Map constains all hashed keys
+type Map struct {
+	hash     Hash
+	replicas int	// 虚拟节点倍数
+	keys     []int // Sorted	hash环	虚拟节点组成的
+	hashMap  map[int]string		//	虚拟节点与真实节点的映射表
+}
+
+// New creates a Map instance
+func New(replicas int, fn Hash) *Map {
+	m := &Map{
+		replicas: replicas,
+		hash:     fn,
+		hashMap:  make(map[int]string),
+	}
+	if m.hash == nil {
+		m.hash = crc32.ChecksumIEEE
+	}
+	return m
+}
+
+// Add adds some keys to the hash.
+func (m *Map) Add(keys ...string) {
+	for _, key := range keys {
+		for i := 0; i < m.replicas; i++ {
+			realKey := strconv.Itoa(i)+key
+			hash := int(m.hash([]byte(realKey)))
+			m.keys = append(m.keys, hash)
+			m.hashMap[hash] = key
+		}
+	}
+	sort.Ints(m.keys)
+}
+
+// Get gets the closest item in the hash to the provided key.
+func (m *Map) Get(key string) string {
+	if len(m.keys) == 0 {
+		return ""
+	}
+
+	hash := int(m.hash([]byte(key)))
+	// Binary search for appropriate replica.
+	idx := sort.Search(len(m.keys), func(i int) bool {
+		return m.keys[i] >= hash
+	})
+	a :=idx%len(m.keys)	//计算新数据需要所在环的位置
+	b :=m.keys[a]	//
+	ret := m.hashMap[b]
+	return ret
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module Go-Learning-Project
+
+go 1.14
+
+require github.com/golang/protobuf v1.4.3
