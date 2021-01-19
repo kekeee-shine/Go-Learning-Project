aee9a2019b93d9ad94eb13a3df35dd2cd5983038
yangfan <fan.yang@polixir.ai>
2021-01-19 10:10:20 +0800

    提交 gee rpc

---
+++ gee-rpc/day1-codec/codec/gob.go
@@ -0,0 +1,59 @@
+package codec
+
+import (
+	"bufio"
+	"encoding/gob"
+	"io"
+	"log"
+)
+
+type GobCodec struct {
+	conn io.ReadWriteCloser
+	buf  *bufio.Writer
+	dec  *gob.Decoder
+	enc  *gob.Encoder
+}
+
+var _ Codec = (*GobCodec)(nil)
+
+func NewGobCodec(conn io.ReadWriteCloser) Codec {
+	buf := bufio.NewWriter(conn)
+	return &GobCodec{
+		conn: conn,
+		buf:  buf,
+		dec:  gob.NewDecoder(conn),
+		enc:  gob.NewEncoder(buf),
+	}
+}
+
+func (c *GobCodec) ReadHeader(h *Header) error {
+	aa :=c.dec.Decode(h)
+	return aa
+}
+
+func (c *GobCodec) ReadBody(body interface{}) error {
+	aa :=c.dec.Decode(body)
+	return aa
+}
+
+func (c *GobCodec) Write(h *Header, body interface{}) (err error) {
+	defer func() {
+		_ = c.buf.Flush()
+		if err != nil {
+			_ = c.Close()
+		}
+	}()
+	if err = c.enc.Encode(h); err != nil {
+		log.Println("rpc: gob error encoding header:", err)
+		return
+	}
+	if err = c.enc.Encode(body); err != nil {
+		log.Println("rpc: gob error encoding body:", err)
+		return
+	}
+	return
+}
+
+func (c *GobCodec) Close() error {
+	return c.conn.Close()
+}
